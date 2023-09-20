417ebb59f1049a11447aca8d2b5d1b71658b84d2
mostafa aboheaba <mostafa.khalile.aboheaba@gmial.com>
2023-09-20 14:30:43 +0300

    add request body for login

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -5,9 +5,10 @@ import 'package:wsc_auth/utilities/api_client.dart';
 
 class Login {
   static ApiClient apiClient = ApiClient.instance;
-  static Future<Response<dynamic>?> login({String path = 'login'}) async {
+  static Future<Response<dynamic>?> login(
+      {String path = 'login', Object body = const {}}) async {
     try {
-      final response = await apiClient.post(path);
+      final response = await apiClient.post(path, body: body);
       return response;
     } catch (e, st) {
       log(e.toString(), stackTrace: st);

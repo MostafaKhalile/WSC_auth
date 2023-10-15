b22299c51d3fb93bd258ed817c81957dc8295f25
mostafa aboheaba <mostafa.khalile.aboheaba@gmial.com>
2023-10-15 09:51:31 +0300

    start

--- lib/src/features/Login/data/data.dart
+++ lib/src/features/Login/data/data.dart
@@ -1,2 +1,2 @@
 export 'models/jwt_token_model.dart';
-export 'repositories/login_repository_imp.dart';
+export 'repositories/login_repository_impl.dart';
--- lib/src/features/Login/data/repositories/login_repository_impl.dart
+++ lib/src/features/Login/data/repositories/login_repository_impl.dart
@@ -6,6 +6,7 @@ import '../../domain/repositories/login_repository.dart';
 import '../../domain/usecases/login_with_email.dart';
 
 class LoginRepositoryImp implements LoginRepository {
+  // final apiDatasource = LogInAPIDataSourceImpl();
   @override
   Future<Either<Failure, JWTToken>> loginWithEmail(Params params) {
     // TODO: implement loginWithEmail

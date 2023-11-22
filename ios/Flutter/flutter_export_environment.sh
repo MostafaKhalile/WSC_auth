41a72c99c733bff944d501a6611d000d4c8fc980
mostafa aboheaba <mostafa.khalile.aboheaba@gmial.com>
2023-11-22 16:24:37 +0200

    login

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -3,7 +3,7 @@
 export "FLUTTER_ROOT=C:\src\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Users\mosta\Development\Multi_vendor_showcase\wsc_auth"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.2"
 export "FLUTTER_BUILD_NUMBER=0.0.2"

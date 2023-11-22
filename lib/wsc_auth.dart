bdf5a6a0d75a8daef49a36f5a8ef80566681509f
mostafa aboheaba <mostafa.khalile.aboheaba@gmial.com>
2023-11-22 16:40:50 +0200

    export Login Module

--- lib/wsc_auth.dart
+++ lib/wsc_auth.dart
@@ -2,6 +2,11 @@ library wsc_auth;
 
 export 'package:wsc_api_client/wsc_api_client.dart';
 
+export 'src/features/login/data/data.dart';
+export 'src/features/login/domain/entities/jwt_token.dart';
+export 'src/features/login/domain/usecases/login_with_email.dart';
+export 'src/features/login/presentation/presentation.dart';
+
 export 'src/features/signup/data/data.dart';
 export 'src/features/signup/domain/domain.dart';
 export 'src/features/signup/presentation/presentation.dart';
